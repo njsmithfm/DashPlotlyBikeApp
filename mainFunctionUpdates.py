e1ac34befb4c0ceb3a6da84553842e232094d693
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2025-04-12 12:27:45 -0400

    feat: fix map legend label

--- mainFunctionUpdates.py
+++ mainFunctionUpdates.py
@@ -27,6 +27,7 @@ borough_crashSums = df.groupby('borough')['number_of_cyclist_injured'].sum().res
 
 def create_map_fig(df, days):  
         map_fig = px.density_map(df, lat='latitude', lon='longitude', z='number_of_cyclist_injured', radius=10,
+                                 labels={'number_of_cyclist_injured': 'Cyclists Injured'},
                                 center=dict(lat = 40.7128, lon = -73.9560), zoom = 10,
                                 map_style = "open-street-map",
                                 title = f'Cyclist Injury Locations (Last {days} Days)')
@@ -47,9 +48,17 @@ def create_line_fig(df, days):
                                  labels={'crash_date': 'Date', 'number_of_cyclist_injured': 'Cyclist Injuries'},)
         return line_fig
 
+# def create_counter_fig(df, days):
+#       counter_fig = 
+
+#       return counter_fig
+
 bar_fig = create_bar_fig(df, initial_days)
 map_fig=create_map_fig(df, initial_days)
 line_fig = create_line_fig(df, initial_days)
+# counter_fig = create_counter_fig(df, initial_days)
+
+
 
 app.layout = html.Div([
     dbc.Container([
@@ -83,11 +92,23 @@ app.layout = html.Div([
                     style={'height': '65vh'})
             ], className="mb-4")
         ]),
+         dbc.Row([
+                dbc.Col([
+            dcc.Graph(id='line-chart', figure=line_fig, responsive=True,
+                style={'height': '50vh'})
+        ], width=12, className="mb-4"),
+        ]),
         dbc.Row([
-            dbc.Col([
-                dcc.Graph(id='line-chart', figure=line_fig, responsive=True,
-                    style={'height': '50vh'})
-            ], width=6, className="mb-4"),
+             dbc.Col([
+                dbc.Card([ 
+                   dbc.CardBody([
+                        html.H5("Counter Card", className="card-title", id='counter-card',
+                            # figure=counter_fig,
+                            )]),
+                        html.P("This is some card content that we'll reuse", className="card-text",             
+            )], className="bg-dark text-white")
+        ],  width=6, className="mb-4"
+    ),
             dbc.Col([
                 dcc.Graph(id='bar-chart', figure=bar_fig, responsive=True, 
                     style={'height': '50vh'})
@@ -104,6 +125,14 @@ def update_map_time(number_of_days):
     updated_map_chart = create_map_fig(df=df, days=number_of_days)
     return updated_map_chart
 
+@app.callback(
+    Output('line-chart', 'figure'),
+    Input('timeframe-dropdown','value')
+)
+def update_line_time(number_of_days):
+    updated_line_chart= create_line_fig(df=df, days=number_of_days)
+    return updated_line_chart
+
 
 @app.callback(
     Output('bar-chart', 'figure'),
@@ -113,14 +142,14 @@ def update_bar_time(number_of_days):
     updated_bar_chart = create_bar_fig(df=df, days=number_of_days)
     return updated_bar_chart
 
+# @app.callback(
+#       Output('counter-card', 'figure'),
+#       Input('timeframe-dropdown', 'value')
+# )
+# def update_counter_card(number_of_days):
+#     updated_counter_card = create_counter_fig(df=df, days=number_of_days)
+#     return updated_counter_card
 
-@app.callback(
-    Output('line-chart', 'figure'),
-    Input('timeframe-dropdown','value')
-)
-def update_line_time(number_of_days):
-    updated_line_chart= create_line_fig(df=df, days=number_of_days)
-    return updated_line_chart
 
 
 if __name__ == '__main__':
