ecc541a3e450fa00635f888621deba036aee520d
njsmithfm <njsmith.fm@gmail.com>
2025-09-03 09:17:42 -0400

    refactor: reduce requirements.txt only to essentials

--- credits.py
+++ credits.py
@@ -1,3 +1,5 @@
+from dash import Dash, dcc, html, Input, Output, callback
+
 html.Div(
     [
         "This app was created through the ",
@@ -6,6 +8,7 @@ html.Div(
             href="https://datavisualizationsociety.org",
             target="_blank",
         ),
-        " Spring 2025 mentorship program. It would not exist without the guidance of my mentor, Adam Kulidjian, and his skill helping me navigate Plotly, Dash, ",
+        " Spring 2025 mentorship program. It would not exist without the guidance of my mentor, Adam Kulidjian, and his skill helping me navigate Plotly and Dash.",
     ],
 ),
+
